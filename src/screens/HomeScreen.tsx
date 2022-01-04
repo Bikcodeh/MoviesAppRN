e706fa55752b1f467c436267139aee42c1be031a
Bikcode <bikcodeh@gmail.com>
2022-01-04 16:24:09 -0500

    implements interface for handle properly response from service

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -4,14 +4,15 @@ import { useNavigation } from '@react-navigation/core';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../navigation/Navigation';
 import movieDB from '../api/MovieDB';
+import { MovieDBNowPlaying } from '../interfaces/movieInterface';
 
 type homeScreenProp = StackNavigationProp<RootStackParamList, 'Home'>
 
 export const HomeScreen = () => {
 
     useEffect(() => {
-        movieDB.get('/now_playing')
-            .then( resp => console.log(resp.data))
+        movieDB.get<MovieDBNowPlaying>('/now_playing')
+            .then( resp => console.log(resp.data.results[0].title))
     }, [])
 
     const navigation = useNavigation<homeScreenProp>()
