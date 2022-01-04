659d8bef7d34d5a1af91d01bc09bd262f7454220
Bikcode <bikcodeh@gmail.com>
2022-01-04 16:17:19 -0500

    create module axios and get movies in Home Screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Button, Text, View } from 'react-native'
 import { useNavigation } from '@react-navigation/core';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../navigation/Navigation';
+import movieDB from '../api/MovieDB';
 
 type homeScreenProp = StackNavigationProp<RootStackParamList, 'Home'>
 
 export const HomeScreen = () => {
+
+    useEffect(() => {
+        movieDB.get('/now_playing')
+            .then( resp => console.log(resp.data))
+    }, [])
 
     const navigation = useNavigation<homeScreenProp>()
 
