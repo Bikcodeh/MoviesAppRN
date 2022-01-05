9d44740ff2d0bd52a252fa2f5e7499bea9ecbc53
Bikcode <bikcodeh@gmail.com>
2022-01-04 19:41:40 -0500

    implements carousel for display currently movies on cinemas

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,25 +1,38 @@
 import React from 'react'
-import { ActivityIndicator, View } from 'react-native'
+import { ActivityIndicator, Dimensions, View } from 'react-native'
+
+import Carousel from 'react-native-snap-carousel';
+
 import { useMovies } from './../hooks/useMovies';
 import { MoviePoster } from '../components/MoviePoster';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+const { width: windowWidth } = Dimensions.get('window');
 
 export const HomeScreen = () => {
 
     const { top } = useSafeAreaInsets()
     const { moviesCurrently, isLoading } = useMovies()
 
-    if(isLoading) {
+    if (isLoading) {
         return (
-            <View style= {{ flex: 1, justifyContent: 'center', alignContent: 'center' }}>
+            <View style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}>
                 <ActivityIndicator color='red' size={50} />
             </View>
         )
     }
 
     return (
-        <View style={{ marginTop: top + 20}}>
-            <MoviePoster movie={ moviesCurrently[0] } />
+        <View style={{ marginTop: top + 20 }}>
+            <View style={{ height: 440 }}>
+                {/* {<MoviePoster movie={ moviesCurrently[0] } />} */}
+                <Carousel
+                    data={moviesCurrently}
+                    renderItem={({ item }: any) => <MoviePoster movie={item} />}
+                    sliderWidth={windowWidth}
+                    itemWidth={300}
+                />
+            </View>
         </View>
     )
 }
