19adc336052ea280a9197255b0e8778f8bdc6473
Bikcode <bikcodeh@gmail.com>
2022-01-05 14:35:34 -0500

    display overview and budge with library format currency

--- src/api/MovieDB.tsx
+++ src/api/MovieDB.tsx
@@ -4,7 +4,7 @@ const movieDB = axios.create({
     baseURL: 'https://api.themoviedb.org/3/movie',
     params: {
         api_key: '4662e7a7fe13c9d91c80552e10a09dc1',
-        language: 'es-ES'
+        language: 'en-EN'
     }
 });
 
--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
+import currencyFormatter from 'currency-formatter';
+
 import { Cast } from '../interfaces/creditsInterface';
 import { MovieFull } from '../interfaces/movieInterface';
 
@@ -22,6 +24,21 @@ export const MovieDetails = ({ movieFull, cast }: Props) => {
                     <Text style={{ marginLeft: 5 }}>{movieFull.vote_average}</Text>
                     <Text style={{ marginLeft: 5 }}>- {movieFull.genres.map(genre => genre.name).join(',')}</Text>
                 </View>
+
+                {/* History */}
+                <Text style={{ fontSize: 24, marginTop: 10, fontWeight: 'bold' }}>
+                    History
+                </Text>
+                <Text style={{ fontSize: 16 }}>
+                    {movieFull.overview}
+                </Text>
+                <Text style={{ fontSize: 24, marginTop: 10, fontWeight: 'bold' }}>
+                    Budget
+                </Text>
+                <Text style={{ fontSize: 18 }}>
+                    {currencyFormatter.format(movieFull.budget, { code: 'USD' })}
+                </Text>
+
             </View>
             { /* Casting */}
         </>
